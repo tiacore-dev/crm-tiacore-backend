a7cd1fcf6280743e8eda0b18627a9e80bb623265
Viktoria <v.s.polikanova@gmail.com>
2025-03-06 18:05:18 +0700

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, R
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
