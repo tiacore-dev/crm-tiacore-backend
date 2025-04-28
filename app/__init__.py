a25e7f54086fc7fa709297c3c41a50707a95a928
Viktoria <v.s.polikanova@gmail.com>
2025-04-28 12:17:12 +0700

    Еще обработчики и отсутствие корс для тестов

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+import traceback
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.exceptions import RequestValidationError
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR, HTTP_422_UNPROCESSABLE_ENTITY
+from loguru import logger
 from prometheus_client import make_asgi_app
 from tortoise.contrib.fastapi import register_tortoise
 from app.logger import setup_logger
@@ -15,6 +18,7 @@ from app.config import Settings
 def create_app(config_name) -> FastAPI:
     app = FastAPI(title="CRM")
     settings = Settings()
+
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[settings.ORIGIN],
@@ -29,9 +33,11 @@ def create_app(config_name) -> FastAPI:
         init_tracer(app)
 
     if config_name == 'Test':
+        origin = "*"
         db_url = settings.TEST_DATABASE_URL
     else:
         db_url = settings.DATABASE_URL
+        origin = settings.ORIGIN
     register_tortoise(
         app,
         db_url=db_url,
@@ -46,7 +52,6 @@ def create_app(config_name) -> FastAPI:
 
     @app.exception_handler(HTTPException)
     async def http_exception_handler(request: Request, exc: HTTPException):
-        from loguru import logger
         logger.error(
             f"HTTPException: {exc.status_code} - {exc.detail} на {request.url}")
 
@@ -55,8 +60,32 @@ def create_app(config_name) -> FastAPI:
             content={"detail": exc.detail},
         )
         # Принудительно вставляем CORS заголовки
-        response.headers["Access-Control-Allow-Origin"] = settings.ORIGIN
+        response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Access-Control-Allow-Credentials"] = "true"
         return response
 
+    @app.exception_handler(Exception)
+    async def global_exception_handler(request: Request, exc: Exception):
+        logger.error(f"Unhandled Exception: {traceback.format_exc()}")
+
+        response = JSONResponse(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"detail": "Internal Server Error"},
+        )
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        return response
+
+    @app.exception_handler(RequestValidationError)
+    async def validation_exception_handler(request: Request, exc: RequestValidationError):
+        logger.error(
+            f"Request Validation Error: {exc.errors()} | body: {exc.body}")
+
+        response = JSONResponse(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"detail": exc.errors()},
+        )
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        return response
     return app
