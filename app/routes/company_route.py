2b9b9981d35cbf4723f49c44469ca02864640df1
Viktoria <v.s.polikanova@gmail.com>
2025-04-30 16:17:50 +0700

    война с кэшем не закончилась (пробою обновлять вручную)

--- app/routes/company_route.py
+++ app/routes/company_route.py
@@ -1,9 +1,11 @@
 from uuid import UUID
 from fastapi import APIRouter, Depends, Path, HTTPException, Body, status
 from loguru import logger
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 from tortoise.expressions import Q
 from app.dependencies.permissions import with_exact_company_permission
-from app.handlers.auth import get_current_user, invalidate_user_cache, get_cached_user_data
+from app.handlers.auth import get_current_user, invalidate_user_cache, get_cached_user_data, get_company_permissions_for_user, custom_key_builder
 from app.database.models import Company, UserCompanyRelation,  UserRole, User
 from app.pydantic_models.company_models import (
     CompanyCreateSchema, CompanyEditSchema, company_filter_params, CompanyResponseSchema, CompanyListResponseSchema, CompanySchema
@@ -30,7 +32,22 @@ async def add_company(data: CompanyCreateSchema = Body(), user_data: dict = Depe
         if role and user:
             await UserCompanyRelation.create(role=role, company=company, user=user)
             await invalidate_user_cache(user.email)
-            await get_cached_user_data(user.email)
+            user_refreshed = await User.get_or_none(email=user.email)
+            if user_refreshed:
+                # Дождись реальной полной записи перед кешированием
+                permissions = await get_company_permissions_for_user(user_refreshed)
+                backend: RedisBackend = FastAPICache.get_backend()
+                key = custom_key_builder(get_cached_user_data, args=[
+                    user.email], kwargs={})
+                value = {
+                    "email": user.email,
+                    "permissions": permissions,
+                    "is_superadmin": user.is_superadmin,
+                    "user_id": user.user_id
+                }
+                # Ставим вручную
+                await backend.set(key, value, expire=300)
+
         return {"company_id": str(company.company_id)}
 
     except (KeyError, TypeError, ValueError) as e:
