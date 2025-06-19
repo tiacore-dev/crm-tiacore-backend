9a6881c8b6554f54f5f82ab945c1d031d46c9ed8
Viktoria <v.s.polikanova@gmail.com>
2025-06-19 17:22:15 +0700

    включила все нужные роуты

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from tiacore_lib.routes.auth_route import auth_router
+from tiacore_lib.routes.company_route import company_router
+from tiacore_lib.routes.invite_route import invite_router
+from tiacore_lib.routes.register_route import register_router
+from tiacore_lib.routes.role_route import role_router
+from tiacore_lib.routes.user_route import user_router
 
 from .act_detail_route import act_detail_router
 from .act_route import act_router
@@ -8,12 +14,20 @@ from .bill_route import bill_router
 from .contract_route import contract_router
 from .easter_route import easter_router
 from .get_route import get_router
+from .legal_entity_route import entity_router
 from .service_route import service_router
 from .template_route import template_router
 
 
 def register_routes(app: FastAPI):
-    app.include_router(get_router, prefix="/api", tags=["Statuses, Types"])
+    app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+    app.include_router(invite_router, prefix="/api", tags=["Invite"])
+    app.include_router(register_router, prefix="/api", tags=["Register"])
+    app.include_router(user_router, prefix="/api/users", tags=["Users"])
+    app.include_router(company_router, prefix="/api/companies", tags=["Companies"])
+    app.include_router(role_router, prefix="/api/roles", tags=["Roles"])
+
+    app.include_router(get_router, prefix="/api", tags=["Statuses"])
     app.include_router(service_router, prefix="/api/services", tags=["Services"])
 
     app.include_router(contract_router, prefix="/api/contracts", tags=["Contracts"])
@@ -30,3 +44,6 @@ def register_routes(app: FastAPI):
     )
     app.include_router(template_router, prefix="/api/templates", tags=["Templates"])
     app.include_router(easter_router)
+    app.include_router(
+        entity_router, prefix="/api/legal-entities", tags=["LegalEntities"]
+    )
