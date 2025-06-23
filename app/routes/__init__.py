e5eb11058918ccb57a9e726cdfdf3b9ad985b6ad
Viktoria <v.s.polikanova@gmail.com>
2025-06-23 16:18:25 +0700

    entity types route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -14,6 +14,7 @@ from .bill_route import bill_router
 from .contract_route import contract_router
 from .easter_route import easter_router
 from .entity_company_relation_route import entity_relation_router
+from .entity_types_route import entity_types_router
 from .get_route import get_router
 from .legal_entity_route import entity_router
 from .service_route import service_router
@@ -50,6 +51,11 @@ def register_routes(app: FastAPI):
     )
     app.include_router(
         entity_relation_router,
+        prefix="/api/entity-company-relations",
+        tags=["EntityCompanyRelations"],
+    )
+    app.include_router(
+        entity_types_router,
         prefix="/api/entity-company-relations",
         tags=["EntityCompanyRelations"],
     )
